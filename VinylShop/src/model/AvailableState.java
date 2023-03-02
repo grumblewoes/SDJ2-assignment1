530f75c3a152bf2f0e7c37136b4198706935dc20
grumblewoes <culturuse@gmail.com>
2023-03-02 13:31:56 +0100

    added getter and setter for borrower, and changed return type during borrow/reserve

--- VinylShop/src/model/AvailableState.java
+++ VinylShop/src/model/AvailableState.java
@@ -5,9 +5,9 @@ public class AvailableState implements LendingState
   @Override public void click(Vinyl vinyl)
   {
     //either to borrow or to reserve
-    if (!vinyl.getBorrowedBy().equals(""))
+    if (!vinyl.getBorrowedBy().equals(null))
       vinyl.setState(new BorrowedState());
-    else if (!vinyl.getReservedBy().equals(""))
+    else if (!vinyl.getReservedBy().equals(null))
       vinyl.setState(new ReservedState());
   }
 }
--- VinylShop/src/model/BorrowedState.java
+++ VinylShop/src/model/BorrowedState.java
@@ -4,9 +4,9 @@ public class BorrowedState implements LendingState
 {
   @Override public void click(Vinyl vinyl)
   {
-    if (vinyl.getBorrowedBy().equals(""))
+    if (vinyl.getBorrowedBy().equals(null))
       vinyl.setState(new AvailableState());
-    else if (!vinyl.getReservedBy().equals(""))
+    else if (!vinyl.getReservedBy().equals(null))
       vinyl.setState(new ReservedWhileBorrowedState());
 
   }
